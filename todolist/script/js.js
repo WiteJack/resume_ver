4025c736142709d122c757d0d62699097897d4f4
WiteJack <witejack177@gmail.com>
2022-02-17 13:16:03 +0300

    1

---
+++ todolist/script/js.js
@@ -0,0 +1,35 @@
+Vue.createApp({
+    data(){
+        return{
+            valueInput:'',
+            needDoList: [],
+            completeList: []
+        };
+    },
+    methods: {
+        hadlyInput(event) {
+            this.valueInput = event.target.value;
+        },
+        addTask (){
+            if(this.valueInput === ''){ return };
+            this.needDoList.push({
+                title: this.valueInput, 
+                id: Math.random()
+            });
+            this.valueInput = '';
+        },
+        doCheck (index, type){
+            if(type === 'need'){
+                const completeMask = this.needDoList.splice(index, 1);
+                this.completeList.push(...completeMask);
+            } else {
+                const nocompleteMask = this.completeList.splice(index, 1);
+                this.needDoList.push(...nocompleteMask);
+            }
+        },
+        removeMask(index, type){
+            const toDolist = type === 'need' ? this.needDoList : this.completeList;
+            toDolist.splice(index, 1);
+        }
+    }
+}).mount('#app');
